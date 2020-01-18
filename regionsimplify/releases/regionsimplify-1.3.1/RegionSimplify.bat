64ee1ef6ef81a796bcf32de8ed10d61cd5e9ab0e
jgaffuri <jgaf@protonmail.com>
2020-01-18 11:28:06 +0100

    new structure

--- regionsimplify/releases/regionsimplify-1.3.1/RegionSimplify.bat
+++
@@ -1,3 +0,0 @@
-mkdir test_out
-java -jar RegionSimplify.jar -i test_data/bangladesh.shp -o test_out/bangladesh-5M.shp -s 5000000
-java -jar RegionSimplify.jar -i test_data/bangladesh.shp -o test_out/bangladesh-10M.shp -s 10000000
