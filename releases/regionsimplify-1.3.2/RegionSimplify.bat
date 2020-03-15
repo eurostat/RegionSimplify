cbb499ee4a84e69b1e12cae2442d3cce151ba88d
jgaffuri <jgaf@protonmail.com>
2020-03-15 21:53:12 +0100

    prepare new release

--- releases/regionsimplify-1.3.2/RegionSimplify.bat
+++ releases/regionsimplify-1.3.2/RegionSimplify.bat
@@ -1,3 +1,3 @@
 mkdir test_out
-java -jar RegionSimplify.jar -i test_data/bangladesh.shp -o test_out/bangladesh-5M.shp -s 5000000
-java -jar RegionSimplify.jar -i test_data/bangladesh.shp -o test_out/bangladesh-10M.shp -s 10000000
+java -jar RegionSimplify.jar -i test_data/bangladesh.gpkg -o test_out/bangladesh-5M.gpkg -s 5000000
+java -jar RegionSimplify.jar -i test_data/bangladesh.gpkg -o test_out/bangladesh-10M.gpkg -s 10000000
--- releases/regionsimplify-1.3.2/RegionSimplify.sh
+++ releases/regionsimplify-1.3.2/RegionSimplify.sh
@@ -7,6 +7,6 @@ do
 	for scaleM in "5" "10"
 	do
     	echo "Generalisation for "$dataset" - 1:"$scaleM"000000"
-		java -jar RegionSimplify.jar -i "test_data/"$dataset".shp" -o "test_out/"$dataset"-"$scaleM"M.shp" -s $scaleM"000000"
+		java -jar RegionSimplify.jar -i "test_data/"$dataset".gpkg" -o "test_out/"$dataset"-"$scaleM"M.gpkg" -s $scaleM"000000"
 	done
 done
