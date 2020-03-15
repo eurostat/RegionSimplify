25ee84c792f71d045bb8530e80dea7148c03bbfa
jgaffuri <jgaf@protonmail.com>
2020-03-15 21:51:07 +0100

    test case

---
+++ src/test/java/eu/europa/ec/eurostat/regionsimplify/TesselationGeneralisationMainTest.java
@@ -0,0 +1,21 @@
+package eu.europa.ec.eurostat.regionsimplify;
+
+import junit.framework.TestCase;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class TesselationGeneralisationMainTest extends TestCase {
+
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(TesselationGeneralisationMainTest.class);
+	}
+
+	public void test() {
+		//for(String gt : new String[] {"surf","lin","pt"})
+			//GeoDiffJarMain.main(new String[] {"-ini", "src/test/resources/ini_"+gt+".gpkg", "-fin", "src/test/resources/fin_"+gt+".gpkg", "-o", "target/testout/test_"+gt+"/"});
+	}
+
+}
