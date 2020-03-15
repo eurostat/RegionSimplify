1c6a83b2095fbca0145ca27b3b1b8dda8b527db5
jgaffuri <jgaf@protonmail.com>
2020-03-15 22:04:39 +0100

    test

--- src/test/java/eu/europa/ec/eurostat/regionsimplify/TesselationGeneralisationMainTest.java
+++ src/test/java/eu/europa/ec/eurostat/regionsimplify/TesselationGeneralisationMainTest.java
@@ -8,20 +8,21 @@ import junit.framework.TestCase;
  */
 public class TesselationGeneralisationMainTest extends TestCase {
 
-
 	public static void main(String[] args) {
 		junit.textui.TestRunner.run(TesselationGeneralisationMainTest.class);
 	}
 
 	public void test() {
 		for(String s : new String[] {"5","10"})
-			for(String inFormat : new String[] {"gpkg","shp"})
+			//TODO
+			for(String inFormat : new String[] {/*"gpkg",*/"shp"})
 				for(String outFormat : new String[] {"gpkg","shp","geojson"})
-					for(String ds : new String[] {"bangladesh", "chile", "china_mainland", "indonesia", "panama", "philippines"})
+					for(String ds : new String[] {"bangladesh", "chile", "china_mainland", "indonesia", "panama", "philippines"}) {
 						TesselationGeneralisationMain.main(new String[] {
 								"-i", "src/test/resources/"+ds+"."+inFormat,
 								"-o", "target/testout/"+ds+"_"+s+"_"+inFormat+"."+outFormat,
 								"-s", s+"000000"});
+					}
 	}
 
 }
