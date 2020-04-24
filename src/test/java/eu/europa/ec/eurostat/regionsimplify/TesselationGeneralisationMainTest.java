9e7ed37eb8699d71a6e3c63e93803857b635b8b7
jgaffuri <jgaf@protonmail.com>
2020-04-24 16:52:43 +0200

    new test

--- src/test/java/eu/europa/ec/eurostat/regionsimplify/TesselationGeneralisationMainTest.java
+++ src/test/java/eu/europa/ec/eurostat/regionsimplify/TesselationGeneralisationMainTest.java
@@ -13,15 +13,22 @@ public class TesselationGeneralisationMainTest extends TestCase {
 	}
 
 	public void test() {
-		for(String s : new String[] {"5","10"})
-			//TODO
-			for(String inFormat : new String[] {/*"gpkg",*/"shp"})
-				for(String outFormat : new String[] {"gpkg","shp","geojson"})
-					for(String ds : new String[] {"bangladesh", "chile", "china_mainland", "indonesia", "panama", "philippines"}) {
+		//TODO
+		for(String inFormat : new String[] {/*"gpkg",*/"shp"})
+			for(String outFormat : new String[] {"gpkg","shp","geojson"})
+				for(String s : new String[] {"1","5","10","50"})
+					for(String ds : new String[] {"chile", "bangladesh", "china_mainland", "indonesia", "panama", "philippines"}) {
 						TesselationGeneralisationMain.main(new String[] {
 								"-i", "src/test/resources/"+ds+"."+inFormat,
-								"-o", "target/testout/"+ds+"_"+s+"_"+inFormat+"."+outFormat,
+								"-o", "target/testout/"+ds+"_1_"+s+"_"+inFormat+"."+outFormat,
+								"-inb", "3",
 								"-s", s+"000000"});
+						/*TesselationGeneralisationMain.main(new String[] {
+								"-i", "src/test/resources/"+ds+"."+inFormat,
+								"-p", "0",
+								"-o", "target/testout/"+ds+"_0_"+s+"_"+inFormat+"."+outFormat,
+								"-inb", "5",
+								"-s", s+"000000"});*/
 					}
 	}
 
