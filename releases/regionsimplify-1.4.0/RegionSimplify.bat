cf57832413343dca4c6c3b945f74935147f17916
jgaffuri <jgaf@protonmail.com>
2020-04-27 16:33:31 +0200

    new version

--- releases/regionsimplify-1.4.0/RegionSimplify.bat
+++
@@ -1,3 +0,0 @@
-mkdir test_out
-java -jar RegionSimplify.jar -i test_data/bangladesh.gpkg -o test_out/bangladesh-5M.gpkg -s 5000000
-java -jar RegionSimplify.jar -i test_data/bangladesh.gpkg -o test_out/bangladesh-10M.gpkg -s 10000000
